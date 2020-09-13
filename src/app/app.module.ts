f5fe136ff39724de53f6156d9e1fef0f87a1dafe
Edison <eandresk.13@hotmail.com>
2020-09-13 11:03:21 -0500

    Se agrega navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './modules/utilities/footer/footer.component';
+import { NavbarComponent } from './modules/utilities/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
